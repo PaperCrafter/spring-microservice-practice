8a2f2123314d9a57bbd6ae87ea9ba6180dd3d5fc
PaperCrafter <leejonghee342@gmail.com>
2020-12-02 20:50:57 +0900

    feat(RandomGeneratorServiceImpl): add RandomGnearator implementation class

---
+++ microservice.practice/src/main/java/com/papercrafter/microservice/practice/service/impl/RandomGeneratorServiceImpl.java
@@ -0,0 +1,17 @@
+package com.papercrafter.microservice.practice.service.impl;
+
+import com.papercrafter.microservice.practice.service.RandomGeneratorService;
+import org.springframework.stereotype.Service;
+
+import java.util.Random;
+
+@Service
+public class RandomGeneratorServiceImpl implements RandomGeneratorService {
+    private static final int MINIMUM_FACTOR = 11;
+    private static final int MAXIMUM_FACTOR = 99;
+
+    @Override
+    public int generateRandomFactor() {
+        return new Random().nextInt((MAXIMUM_FACTOR - MINIMUM_FACTOR) + 1) + MINIMUM_FACTOR;
+    }
+}
