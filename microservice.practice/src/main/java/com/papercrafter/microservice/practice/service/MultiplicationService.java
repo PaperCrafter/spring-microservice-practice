4de532f6c668e1ff7cfb62772a664fe349773821
PaperCrafter <leejonghee342@gmail.com>
2020-12-02 22:19:20 +0900

    add MultiplicationService new interface
    added MultiplicationService new interface
    - checkAttempt: check if multiplication of member variables of Multiplication equals resultAttempt

--- microservice.practice/src/main/java/com/papercrafter/microservice/practice/service/MultiplicationService.java
+++ microservice.practice/src/main/java/com/papercrafter/microservice/practice/service/MultiplicationService.java
@@ -1,7 +1,10 @@
 package com.papercrafter.microservice.practice.service;
 
 import com.papercrafter.microservice.practice.domain.Multiplication;
+import com.papercrafter.microservice.practice.domain.MultiplicationResultAttempt;
 
 public interface MultiplicationService {
     Multiplication createMultiplication();
+    
+    boolean checkAttempt(final MultiplicationResultAttempt resultAttempt);
 }
